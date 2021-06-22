b82ce49460d9254368c861bc43975bffb40591e2
Josh Motter <joshmotter@MacBook-Pro.fios-router.home>
2021-06-22 14:36:55 -0400

    ternary operators

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
-// Strict Equality (Type + Value)
-console.log(1 === 1);
-console.log('1' === 1);
+// If a customer has more than 100 points,
+// they are a 'gold' customer, otherwise,
+// they are a 'silver' customer.
 
-//Loose Equality (Value only)
-console.log(1 == 1);
-console.log('1' == 1);
-console.log(true == 1);
+let points = 90;
+let type = points > 100 ? 'gold' : 'silver';
+
+console.log(type);
